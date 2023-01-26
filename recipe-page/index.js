782ba21439de689598b590a121f1ed043b75959d
Mifrah Malik <mifrah.malik@gmail.com>
2023-01-26 14:37:10 -0500

    js dom incorporated

--- recipe-page/index.js
+++ recipe-page/index.js
@@ -6,3 +6,37 @@ newListItem.textContent = '1 tsp paprika';
 
 // And finally, let's add that list item as a child of the ul.
 document.querySelector('ul').appendChild(newListItem);
+
+//fetch back to myDiv
+const myDiv = document.getElementById("myDiv");
+console.log(myDiv);
+
+//creating checkbox element
+const checkbox = document.createElement("input")
+
+//assigning attributes 
+checkbox.type = "checkbox";
+checkbox.name = "name";
+checkbox.value = "value";
+checkbox.id = "id";
+
+//creating label for checkbox
+var label = document.createElement("label");
+
+//assocciate lable using for html, links back to line 21
+label.htmlFor ="id";
+
+//appending
+label.appendChild(
+    document.createTextNode("This creates label for checkbox.")
+);
+
+//appending the checkbox
+// and label to div
+myDiv.appendChild(checkbox);
+myDiv.appendChild(label);
+
+//add event listener to label
+label.addEventListener("click",(event)=>{
+    event.target.classList.toggle("checked")
+});
